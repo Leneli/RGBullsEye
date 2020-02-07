8f90e1ee838933f579799dd0e110a2537a1b30d9
Elena Kuklina <kuklina@mail.tvil.ru>
2020-02-07 18:52:15 +0400

    Initial Commit

---
+++ RGBullsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RGBullsEye
+//
+//  Created by  Elena Kuklina on 07.02.2020.
+//  Copyright © 2020  Elena Kuklina. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
